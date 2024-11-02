6a000a99be5a746e8210f00e8dd3aed2d78b7cbf
caseyjdavis <casey.j.davis@gmail.com>
2024-11-02 09:46:13 -0600

    Added apps and registry settings

--- install/chocolatey.ps1
+++ install/chocolatey.ps1
@@ -1,19 +1,18 @@
 LOG_TITLE "Update chocolatey"
 choco upgrade chocolatey
 
-LOG_TITLE "Install drivers"
-choco install autohotkey -y --ignore-checksums
-
 LOG_TITLE "Install online tools"
 choco install firefox -y --ignore-checksums
 choco install googlechrome -y --ignore-checksums
+choco install obsidian -y --ignore-checksums
 
 LOG_TITLE "Install programming tools"
 choco install vscode -y --ignore-checksums
-choco install nodejs -y --ignore-checksums
-choco install python -y --ignore-checksums
+choco install powershell-core --pre -y --ignore-checksums
+choco install microsoft-windows-terminal --pre -y --ignore-checksums
 
 LOG_TITLE "Install utils"
+choco install autohotkey -y --ignore-checksums
 choco install git -y --ignore-checksums
 choco install winrar -y --ignore-checksums
 
@@ -23,3 +22,9 @@ choco install vlc -y --ignore-checksums
 
 LOG_TITLE "Install fonts"
 choco install nerd-fonts-jetbrainsmono -y --ignore-checksums
+
+LOG_TITLE "Install HP Printer Drivers"
+choco install hp-universal-print-driver-pcl -y --ignore-checksums
+
+LOG_TITLE "Install Steam"
+choco install steam -y --ignore-checksums
--- main.ps1
+++ main.ps1
@@ -21,7 +21,7 @@ LOG_TITLE "POS-INSTALL"
 # LOG_TITLE "REMAP KEY SHORTCUTS"		| .\postinstall\auto_hot_key.ps1
 # LOG_TITLE "SETUP DATE AND TIME"		| .\postinstall\time_date.ps1
 LOG_TITLE "SETUP GIT"				| .\postinstall\git.ps1
-# LOG_TITLE "FIX REGISTERY"			| .\postinstall\registery.ps1
+LOG_TITLE "FIX REGISTERY"			| .\postinstall\registery.ps1
 LOG_TITLE "MANUAL SETUP"			| .\postinstall\manual_setup.ps1
 LOG_TITLE "WSL"         			| .\postinstall\wsl.ps1
 
